ff7b99332535dfa7c5e4246a802660e4068f38ff
Stasiassi <simonovanastasia360@gmail.com>
2023-02-12 20:27:38 +0300

    feat: make brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.join('').replace(/,/g, ''); 
+  let stack = [];
+  for (let i = 0; i < str.length; i++) 
+  {
+    let index = bracketsConfig.indexOf(str[i]);
+    if (index % 2 === 0) 
+    {
+      if (str[i] === bracketsConfig[index + 1] && stack[stack.length - 1] === index) { stack.pop(); } 
+      else { stack.push(index); }
+    } 
+    else if (stack.pop() !== index-1) { return false; }   
+  }
+  return stack.length === 0;
 }
